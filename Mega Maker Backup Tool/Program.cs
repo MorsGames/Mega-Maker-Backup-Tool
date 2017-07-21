4465365b18a86a4f1e752c392666f5ff236417c4
Mors <ueufangaming@gmail.com>
2017-07-21 23:45:08 +0300

    Uploaded the stuff

---
+++ Mega Maker Backup Tool/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Windows.Forms;
+
+namespace Mega_Maker_Backup_Tool
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            try
+            {
+                Application.EnableVisualStyles();
+                var mmDir = Environment.GetEnvironmentVariable("LocalAppData") + @"\MegaMaker";
+                MessageBoxManager.OK = "Load";
+                MessageBoxManager.Yes = "Yep";
+                MessageBoxManager.No = "Nope";
+                MessageBoxManager.Abort = "Backup";
+                MessageBoxManager.Retry = "Restore";
+                MessageBoxManager.Ignore = "Delete";
+                MessageBoxManager.Register();
+                var answer = MessageBox.Show("Do you want to backup, restore, or delete your save data?", "Mega Maker Backup Tool", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1, (MessageBoxOptions)0x40000);
+                if (answer == DialogResult.Abort)
+                {
+                    var fd = new SaveFileDialog();
+                    fd.Filter = "Mega Maker backup files (*.mmb)|*.mmb|All files (*.*)|*.*";
+                    fd.Title = "Export Backup Data";
+                    fd.FileName = "backup.mmb";
+                    if (fd.ShowDialog(new Form { TopMost = true }) == DialogResult.OK)
+                    {
+                        if (File.Exists(fd.FileName))
+                            File.Delete(fd.FileName);
+                        ZipFile.CreateFromDirectory(mmDir, fd.FileName, CompressionLevel.Optimal, false);
+                        MessageBox.Show("Done!", "Mega Maker Backup Tool", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, (MessageBoxOptions)0x40000);
+                    }
+                }
+                if (answer == DialogResult.Retry)
+                {
+                    var fd = new OpenFileDialog();
+                    fd.Filter = "Mega Maker backup files (*.mmb)|*.mmb|All files (*.*)|*.*";
+                    fd.Title = "Import Backup Data";
+                    fd.FileName = "backup.mmb";
+                    if (fd.ShowDialog(new Form { TopMost = true }) == DialogResult.OK)
+                    {
+                        var di = new DirectoryInfo(mmDir);
+                        foreach (FileInfo file in di.GetFiles())
+                            file.Delete();
+                        foreach (DirectoryInfo dir in di.GetDirectories())
+                            dir.Delete(true);
+                        ZipFile.ExtractToDirectory(fd.FileName, mmDir);
+                        MessageBox.Show("Done!", "Mega Maker Backup Tool", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, (MessageBoxOptions)0x40000);
+                    }
+                }
+                if (answer == DialogResult.Ignore)
+                {
+                    if (MessageBox.Show("Do you really want to delete your save data? You won't be able to change your mind later.", "Mega Maker Backup Tool", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1, (MessageBoxOptions)0x40000) == DialogResult.Yes)
+                    {
+                        var di = new DirectoryInfo(mmDir);
+                        foreach (FileInfo file in di.GetFiles())
+                            file.Delete();
+                        foreach (DirectoryInfo dir in di.GetDirectories())
+                            dir.Delete(true);
+                        MessageBox.Show("RIP", "Mega Maker Backup Tool", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, (MessageBoxOptions)0x40000);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBoxManager.Unregister();
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1, (MessageBoxOptions)0x40000);
+            }
+        }
+    }
+}
---
+++ Mega Maker Backup Tool/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Mega Maker Backup Tool")]
+[assembly: AssemblyDescription("A tool that allows you to backup your Mega Maker save data.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mors")]
+[assembly: AssemblyProduct("Mega Maker Backup Tool")]
+[assembly: AssemblyCopyright("Copyright © Mors 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9e18cc18-0878-42a0-a9a3-9e6cc8400392")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
+
